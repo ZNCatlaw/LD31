d294333ff1a7d5fb4424e198a22e2be1f391aa52
Tim Bellefleur <nomoon@phoebus.ca>
2014-12-07 01:08:23 -0800

    They actually exist now!

---
+++ game/ship.lua
@@ -0,0 +1,10 @@
+local class = Class.new('Ship')
+
+function class:initialize()
+end
+
+function class:update(dt)
+end
+
+--
+return class
---
+++ game/station.lua
@@ -0,0 +1,10 @@
+local class = Class.new('Station')
+
+function class:initialize()
+end
+
+function class:update(dt)
+end
+
+--
+return class
