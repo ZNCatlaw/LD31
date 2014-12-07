73431534e975c55c0130c8c41c9d6f7d81c19585
Tim Bellefleur <nomoon@phoebus.ca>
2014-12-06 18:26:38 -0800

    Real sound and interactivity

---
+++ game/images.lua
@@ -0,0 +1,9 @@
+local images = {}
+
+images['background'] = love.graphics.newImage('assets/images/title-bg.png')
+images['snowman'] = love.graphics.newImage('assets/images/title-snowman.png')
+images['titletext'] = love.graphics.newImage('assets/images/title-titletext.png')
+images['znc'] = love.graphics.newImage('assets/images/title-znc.png')
+images['clicktostart'] = love.graphics.newImage('assets/images/title-clicktostart.png')
+
+game.images = images
--- game/states/title_state.lua
+++ game/states/title_state.lua
@@ -1,8 +1,5 @@
 local state = {}
 
-local timer = hump.Timer.new()
-local drawImages = {}
-
 local function playTitleMusic()
   TEsound.playLooping("assets/sounds/music-roygbiv.mp3", "music")
 end
@@ -11,24 +8,31 @@ function state:init()
 end
 
 function state:enter()
-    count = 0
-    drawImages = {}
+    self.timer = hump.Timer.new()
+    self.drawImages = {}
+    self.interactive =  false
+    self.flash = false
 
-    timer.add(1, playTitleMusic)
-    timer.add(2, function() table.insert(drawImages, game.images.znc) end)
-    timer.add(3, function() table.insert(drawImages, game.images.titletext) end)
+    self.timer.add(1, playTitleMusic)
+    self.timer.add(2, function() table.insert(self.drawImages, game.images.znc) end)
+    self.timer.add(3, function() table.insert(self.drawImages, game.images.titletext) end)
+    self.timer.add(4, function()
+        self.interactive = true
+
+        self.flash = true
+        self.timer.addPeriodic(0.5, function()
+          self.flash = not self.flash
+        end)
+    end)
 end
 
 function state:leave()
-    timer.clear()
+    self.timer.clear()
     TEsound.stop('music')
 end
 
-function state:resume()
-end
-
 function state:update(dt)
-  timer.update(dt)
+    self.timer.update(dt)
 end
 
 function state:draw()
@@ -39,9 +43,17 @@ function state:draw()
     love.graphics.draw(game.images['snowman'])
     love.graphics.setColor(r,g,b,a)
 
-    for _,v in pairs(drawImages) do
+    for _,v in pairs(self.drawImages) do
         love.graphics.draw(v)
     end
+
+    if self.flash then love.graphics.draw(game.images['clicktostart']) end
+end
+
+function state:mousepressed(x, y, button)
+  if (self.interactive and button == 'l') then
+    Gamestate.switch(game.states.play)
+  end
 end
 
 --
